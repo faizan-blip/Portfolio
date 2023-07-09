73e77a0f4f4a7e1a7af5ec309948929bf7d30bf5
faizan-blip <faizanrock753@gmail.com>
2023-07-09 16:24:17 +0530

    backend added fixed

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,11 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 app.use(cors({
-    origin: 'https://faizanak-portfolio.netlify.app/',
+    origin: 'https://faizanak-portfolio.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }));
+  
 app.use(express.json())
 
 const routes = require('./routes/files')
