90406b46eaf819f49316d2965897a8090dedb75c
faizan-blip <faizanrock753@gmail.com>
2023-07-10 22:16:40 +0530

    bug solved

--- backend/routes/files.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-
-const {create} = require('../controllers/create')
-
-router.post('/createdata' , create)
-
-module.exports = router;
