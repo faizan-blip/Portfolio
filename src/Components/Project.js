d404d178f7571bcfc7ff30d7c00e953a9d0a85a5
faizan-blip <faizanrock753@gmail.com>
2023-12-28 18:44:48 +0530

    fixed ui

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -63,7 +63,7 @@ window.addEventListener('scroll', handleScroll);
     const url = 'https://x-penso.vercel.app/';
     window.open(url, '_blank');
   }}   className='hover3'  sx={{minWidth:"300px" , height:"17em" , borderRadius:"18px" , flexGrow:"1" , position:"relative" , background:"none"}}>
-        <img src={xp} className='ft' alt="" width={200} style={{position:"absolute" , top:"45%" , left:"50%" , transform:"translate(-50% , -50%)"}} />
+        <img src={xp} className='xp' alt="" width={200} style={{position:"absolute" , top:"45%" , left:"50%" , transform:"translate(-50% , -50%)"}} />
         <Typography sx={{position:"absolute"  , left:"05%" , top:"10%" , fontSize:"20px" , color:"#fff"}}>Xpenso</Typography>
         <Fab className='hover3'  variant='contained' sx={{position:"absolute" , bottom:"7%" , right:"10%" , background:"none !important" , color:"#fff" , backdropFilter:"blur(2px)" , fontWeight:"700" , borderRadius:"25px" }}><TbBrandNextjs/></Fab>
         <Fab className='hover3'  variant='contained' sx={{position:"absolute" , bottom:"7%" , right:"20%" , background:"none !important" , color:"#fff" , backdropFilter:"blur(2px)" , fontWeight:"700" , borderRadius:"25px" }}><SiExpress/></Fab>
